50222efc66b7d77f1b6e926592b78978d0cd6e96
Luka Milenkovic <milenkovicluka449@gmail.com>
2026-04-17 09:29:18 +0200

    Cleaner 'game' logic

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
-import { bindContext } from "@khankudo/kisdb"
 import { createSQLiteHandle, destroySQLiteHandle } from "@khankudo/kisdb/db/sqlite"
 import { createWebSocketConfig } from "@khankudo/kisdb/server/websocket"
-import { createVanillaViewer, refUpdater } from "@khankudo/kisdb/viewer/vanilla"
+import { createDirectClient } from "@khankudo/kisdb/client/direct"
+import { createVanillaViewer, refUpdater, risingEdge } from "@khankudo/kisdb/viewer/vanilla"
 
 const handle = await createSQLiteHandle('../sqlite')
 
@@ -23,8 +23,8 @@ export type ionowType = {
   position: { x: number, y: number },
   color: number,
 }
-
-const DB = createVanillaViewer<ionowType>(bindContext({ connection: 0, token: Bun.env.SERVER_TOKEN ?? '' }, handle))
+const direct = createDirectClient(handle, { connection: 0, token: Bun.env.SERVER_TOKEN ?? '' })
+const DB = createVanillaViewer<ionowType>(direct)
 
 if (await DB.color === undefined) {
   await DB({
@@ -36,7 +36,7 @@ if (await DB.color === undefined) {
       'middle': false,
     },
     matrix: '0'.repeat(64),
-    color: 0,
+    color: 0o333,
     position: {
       x: 0,
       y: 0,
@@ -48,7 +48,14 @@ function toMatrix(grid: number[]): string {
   return grid.reduce((str, x) => str + String.fromCharCode(48 + ((Math.floor(x / 0o100) << 4) | (Math.floor((x % 0o100) / 0o10) << 2) | (x % 0o10))), '')
 }
 
-refUpdater((pos, col) => {
+let X = await DB.position.x
+DB.position.x.$onnow = x => X = x
+let Y = await DB.position.y
+DB.position.y.$onnow = y => Y = y
+let C = await DB.color
+DB.color.$onnow = c => C = c
+
+refUpdater((x, y, col) => {
   const grid = [
     0o000, 0o002, 0o000, 0o000, 0o000, 0o000, 0o222, 0o000,
     0o002, 0o002, 0o000, 0o000, 0o000, 0o000, 0o222, 0o222,
@@ -60,51 +67,29 @@ refUpdater((pos, col) => {
     0o000, 0o020, 0o000, 0o000, 0o000, 0o000, 0o200, 0o000,
   ]
 
-  //@ts-ignore
-  const index: number = pos.x + (7 - pos.y) * 8
-  //@ts-ignore
+  const index: number = x + (7 - y) * 8
   grid[index] = col
 
   DB.matrix(toMatrix(grid))
-}, DB.position, DB.color)
+}, DB.position.x, DB.position.y, DB.color)
 
-DB.position.$on = p => pos = p
-let pos = await DB.position
-if (!pos)
-  DB.position({ x: 3, y: 3 })
-DB.color.$on = c => col = c
-let col = await DB.color
-if (typeof col !== 'number')
-  DB.color(0o300)
-
-DB.stick.up.$on = (state) => {
-  if (state)
-    DB.position.y((pos.y + 1) % 8)
-}
-DB.stick.down.$on = (state) => {
-  if (state)
-    DB.position.y((pos.y + 8 - 1) % 8)
-}
-DB.stick.right.$on = (state) => {
-  if (state)
-    DB.position.x((pos.x + 1) % 8)
-}
-DB.stick.left.$on = (state) => {
-  if (state)
-    DB.position.x((pos.x + 8 - 1) % 8)
-}
-DB.stick.middle.$on = (state) => {
-  if (state) {
-    if (col === 0)
-      col = 0o333
-    else if (col === 0o333)
-      col = 0o300
-    else
-      col = col >> 3
-
-    DB.color(col)
+DB.stick.up.$onnow = risingEdge(() => DB.position.y((Y + 1) % 8))
+DB.stick.down.$onnow = risingEdge(() => DB.position.y((Y + 8 - 1) % 8))
+DB.stick.right.$onnow = risingEdge(() => DB.position.x((X + 1) % 8))
+DB.stick.left.$onnow = risingEdge(() => DB.position.x((X + 8 - 1) % 8))
+DB.stick.middle.$onnow = risingEdge(() => {
+  switch (C) {
+    case 0o000:
+      DB.color(0o333)
+      break
+    case 0o333:
+      DB.color(0o300)
+      break
+    default:
+      DB.color(C >> 3)
+      break
   }
-}
+})
 
 process.on('exit', () => {
   server.stop(true)
